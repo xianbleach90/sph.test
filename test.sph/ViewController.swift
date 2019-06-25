a8b8e6886e5854ec693f75c396bb8ad0a57d33ff
Liam <liam@propnex.com>
2019-06-25 22:01:33 +0800

    Initial Commit

---
+++ test.sph/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test.sph
+//
+//  Created by it on 25/6/19.
+//  Copyright © 2019 it. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test.sphTests/test_sphTests.swift
@@ -0,0 +1,34 @@
+//
+//  test_sphTests.swift
+//  test.sphTests
+//
+//  Created by it on 25/6/19.
+//  Copyright © 2019 it. All rights reserved.
+//
+
+import XCTest
+@testable import test_sph
+
+class test_sphTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
